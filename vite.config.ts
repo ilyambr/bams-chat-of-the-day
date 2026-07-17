be2a9f9454ecc453455383bf272efce10fad214c
ilyambr <ilyambr@proton.me>
2026-07-17 03:01:18 -0400

    add Cloudflare Worker config and build outDir

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/bams/chat/',
   plugins: [react()],
+  build: {
+    outDir: 'dist/bams/chat',
+    emptyOutDir: true
+  },
   server: {
     proxy: {
       '/youtube-live-chat': {
